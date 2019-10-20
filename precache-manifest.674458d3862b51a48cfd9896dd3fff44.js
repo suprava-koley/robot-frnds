2a363eb9b26085633e18334200de40e15c90e629
Suprava18 <54614920+Suprava18@users.noreply.github.com>
2019-10-20 20:30:07 +0530

    Updates

---
+++ precache-manifest.674458d3862b51a48cfd9896dd3fff44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e7270e949de77523b15942b1e7b4bc",
+    "url": "/robot-frnds/index.html"
+  },
+  {
+    "revision": "b7d07c2e526a868010d4",
+    "url": "/robot-frnds/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dc5f3b285b6f966a5a73",
+    "url": "/robot-frnds/static/css/main.8fb0f544.chunk.css"
+  },
+  {
+    "revision": "b7d07c2e526a868010d4",
+    "url": "/robot-frnds/static/js/2.47d46738.chunk.js"
+  },
+  {
+    "revision": "dc5f3b285b6f966a5a73",
+    "url": "/robot-frnds/static/js/main.85d96e02.chunk.js"
+  },
+  {
+    "revision": "8447e8e6af1535d81fbd",
+    "url": "/robot-frnds/static/js/runtime-main.88b816b2.js"
+  }
+]);
